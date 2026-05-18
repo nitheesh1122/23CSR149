0fee36f8024aeca2b5f34283b83eaa837afb709e
Nitheesh S <nitheeshselvaraj01@gmail.com>
2026-05-18 10:20:01 +0530

    stage 1

---
+++ affordmed-backend/src/app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const cors = require("cors");
+const scheduleRoutes = require("./routes/scheduleroutes");
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/schedule", scheduleRoutes);
+module.exports = app;
